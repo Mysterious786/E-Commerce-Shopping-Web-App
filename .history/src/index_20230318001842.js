e85e0bf2c1645aa8e5cea04e493c625dbd271204
Saqlain Zarjis Ansari <110441918+Mysterious786@users.noreply.github.com>
2023-03-18 01:28:18 +0530

    Added Redux Dev-Tools

---
+++ .history/src/index_20230318001842.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+//Importing react-redux own provider
+import { Provider } from 'react-redux';
+import App from './App';
+// import { UserProvider } from './contexts/user.context';
+// import {CategoriesProvider } from './contexts/categories.context';
+import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
+// import { CartProvider } from './contexts/cart.context';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store,persistor } from './store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+  {/* With this we have setup our reducer */}
+  
+  <Provider store={store}>
+    <PersistGate>
+  <BrowserRouter>
+  {/* user provider tells us inside of my user tree which component have access to my context anything outside will not be able to access*/}
+  {/* user provider can go ino to its children and fetch the data */}
+  {/* <UserProvider> */}
+  {/* <CategoriesProvider>  */}
+  {/* <CartProvider> */}
+  <App />
+  {/* </CartProvider> */}
+  
+  {/* </CategoriesProvider> */}
+ 
+  {/* </UserProvider> */}
+    </BrowserRouter>
+    </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
+
+//we want that our signin form could access this context move to signin.
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ .history/src/store/store_20230318001532.js
@@ -0,0 +1,45 @@
+//This is a place where we receive actions and dispatch them
+//We will be generating our store object here.
+//Every store to work needs reducers
+//root reducer is the combination of all reducers
+import { compose,applyMiddleware,legacy_createStore as createStore } from "redux";
+// import logger from "redux-logger";
+import {rootReducer} from './root-reducer';
+import { persistStore,persistReducer } from "redux-persist";
+import storage from 'redux-persist/storage'
+//It takes three arguments
+//1.rootReducer --> we need a root reducer inorder to generate the store
+//logger states how the store looks like before dispatch and after action
+//Middlewares are like a little library helper that runs before an action hits the reducer
+//so before an action hits the reducer ,it hits the middleware
+//The middlewares actually enhances our store and they log out the state
+//Removinf logger and creating our own loggerMiddleware.
+//Signature of this creation will be same it takes three functions that return from one another(curryFunc)
+
+//Debugging with redux logger...
+
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {return next(action) ;}// we simply returns from this  middleware
+    //But if there is an action then 
+    console.log('type ',action.type);
+    console.log('payload ',action.payload);
+    console.log('currentState', store.getState());//getState() will give us the value of state now
+    next(action);
+    console.log('next state: ',store.getState() ); // NEW STATE
+}
+
+const persistConfig = {
+    key :'root',
+    storage,
+    //blacklisting the user...
+    blacklist: ['user']
+}
+const persistedReducer = persistReducer(persistConfig,rootReducer);
+
+const middlewares = [loggerMiddleware];
+//Passing undefined because it is an optional passed parameter
+const composedEnhancers = compose(applyMiddleware(...middlewares));
+export const store = createStore(persistedReducer,undefined,composedEnhancers);
+
+
+export const persistor = 
